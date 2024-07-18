9784d0432de3ce2718b9e0df27091e4860af8fb5
DarkSharpness <2040703891@qq.com>
2024-07-18 18:17:56 +0800

    test: add fno section anchor to diable gcc-anchor optimization

--- testcases/c/compile.sh
+++ testcases/c/compile.sh
@@ -3,7 +3,7 @@ test_files=$(find . -name "*.c")
 
 # compile all the .c files
 for file in $test_files; do
-    riscv64-linux-gnu-gcc $file -S -march=rv32im -mabi=ilp32 -nostdlib -O2 -o ${file%.c}.s
+    riscv64-linux-gnu-gcc $file -S -march=rv32im -mabi=ilp32 -nostdlib -O2 -o ${file%.c}.s -fno-section-anchors
 done
 
 # remove @plt and @got suffixes from the assembly files
